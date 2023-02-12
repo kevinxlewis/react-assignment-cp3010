6f2ff599179b8898b9152d1af82a48821ba4f3d8
Kevin Lewis <kevinjlewis0@gmail.com>
2023-02-12 14:10:48 -0330

    added nav component and installed React-Router

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <ul>
+                <li>
+                    <a href="/movie-reviews">Movie Reviews</a>
+                </li>
+                <li>
+                <a href="/submit-movie-review">Submit A Review</a>
+                </li>    
+            </ul>
+        </nav>
+    )
+}
